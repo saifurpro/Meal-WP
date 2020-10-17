ff78038c890962d4df750a864431a133c4bf1522
Saifur Rahman <saifur703@gmail.com>
2020-10-17 16:38:58 +0600

    theme option

--- inc/cs-framework/init.php
+++ inc/cs-framework/init.php
@@ -1,14 +1,49 @@
-<?php 
-define( 'CS_ACTIVE_FRAMEWORK',  false  ); // default true
-define( 'CS_ACTIVE_METABOX',    true ); // default true
-define( 'CS_ACTIVE_TAXONOMY',    true ); // default true
-define( 'CS_ACTIVE_SHORTCODE',  false ); // default true
-define( 'CS_ACTIVE_CUSTOMIZE',  false ); // default true
+<?php
+define('CS_ACTIVE_FRAMEWORK', true); // default true
+define('CS_ACTIVE_METABOX', true); // default true
+define('CS_ACTIVE_TAXONOMY', true); // default true
+define('CS_ACTIVE_SHORTCODE', false); // default true
+define('CS_ACTIVE_CUSTOMIZE', false); // default true
 
-define( 'CS_ACTIVE_LIGHT_THEME', true ); // default false
+define('CS_ACTIVE_LIGHT_THEME', true); // default false
 
-function meal_codestar_init() {
+function meal_codestar_init()
+{
     CSFramework_Metabox::instance(array());
     CSFramework_Taxonomy::instance(array());
+
+    $settings = array(
+        'menu_title' => __('Meal Options', 'meal'),
+        'menu_type' => 'menu',
+        'menu_slug' => 'meal-options-panel',
+        'framework_title' => __('Meal Options', 'meal'),
+        'ajax_save' => false,
+        'show_reset_all' => true,
+    );
+    new CSFramework($settings, meal_get_theme_options());
 }
 add_action('init', 'meal_codestar_init');
+
+function meal_get_theme_options()
+{
+    $options = array();
+
+    $options[] = array(
+        'name' => 'meal_theme_activation',
+        'title' => __('Theme Activation', 'meal'),
+        'icon' => 'fa fa-heart',
+        'fields' => array(
+            array(
+                'id' => 'username',
+                'type' => 'text',
+                'title' => __('Username', 'meal'),
+            ),
+            array(
+                'id' => 'purchase_code',
+                'type' => 'text',
+                'title' => __('Purchase Code', 'meal'),
+            ),
+        ),
+    );
+    return $options;
+}
